e9a764d7b837b4df3474036f243f946b0752c3a4
Akanksha <17aks11@gmail.com>
2024-02-29 23:07:46 +0530

    Added tailwind to the application, other baseic imports across components and setups.

--- src/app/components/galactic-system/galactic-system.component.ts
+++ src/app/components/galactic-system/galactic-system.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CelestialBodyComponent } from '../celestial-body/celestial-body.component';
 
 @Component({
   selector: 'app-galactic-system',
   standalone: true,
-  imports: [],
+  imports: [CelestialBodyComponent],
   templateUrl: './galactic-system.component.html',
   styleUrl: './galactic-system.component.css'
 })
