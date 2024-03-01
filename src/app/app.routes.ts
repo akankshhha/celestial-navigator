0e06739a22813669f85ab87283ccce66a0e93ee4
Akanksha <17aks11@gmail.com>
2024-03-01 14:33:05 +0530

    added route for planetology as well.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { GalacticSystemComponent } from './components/galactic-system/galactic-system.component';
+import { PlanetologyComponent } from './planetology/planetology.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/galactic-system', pathMatch: 'full' },
+    { path: 'galactic-system', component: GalacticSystemComponent },
+    { path: 'planet/:name', component: PlanetologyComponent },
+    { path: '**', redirectTo: '/galactic-system' } 
+];
