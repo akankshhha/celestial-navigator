5f48605420c7abeee5d2bdb1d34662b2e065217d
Akanksha <17aks11@gmail.com>
2024-03-03 02:40:31 +0530

    added check for resident name length.

--- src/app/components/cards/resident-profile/resident-profile.component.ts
+++ src/app/components/cards/resident-profile/resident-profile.component.ts
@@ -1,16 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resident-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resident-profile.component.html',
   styleUrl: './resident-profile.component.css'
 })
 export class ResidentProfileComponent implements OnInit {
 
 @Input() residentDetails: any;
+  isLongText: boolean = false;
 
 ngOnInit(): void {
+  if( this.residentDetails.name.length > 10) {
+    this.isLongText = true
+  } 
 }
 }
